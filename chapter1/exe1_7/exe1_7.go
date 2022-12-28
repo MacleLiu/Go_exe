4b9e886532d6bbb9a9dd9b1aee8cbf0191ee1216
Manchun-Liu <1281356585@qq.com>
2022-12-28 21:49:12 +0800

    add exe1_7

---
+++ chapter1/exe1_7/exe1_7.go
@@ -0,0 +1,28 @@
+// fetch输出从URL获取的内容
+// 使用函数io.Copy(dst,src)代替ioutil.ReadAll来复制响应内容到os.Stdout
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		//b, err := ioutil.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Fprintln(os.Stdout)
+	}
+}
